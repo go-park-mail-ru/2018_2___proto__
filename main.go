2638a2d02a264ec254871bec4c0aa63c2b9df12f
0sektor0 <tomskiy_a@list.ru>
2018-10-01 01:04:12 +0300

    исправил баг с двойным обработчиком

--- main.go
+++ main.go
@@ -15,16 +15,14 @@ func main() {
 	apiHandler := NewApiHandler(cfg)
 
 	//урлы должны быть отсортированы по длине урла по убыванию потом жобавлю это программно
-	router.AddHandlerPost("/signin", apiHandler.CorsEnableMiddleware(apiHandler.Authorize))
 	router.AddHandlerGet("/user/{slug}", apiHandler.CorsEnableMiddleware(apiHandler.AuthMiddleware(apiHandler.GetUser)))
 	router.AddHandlerGet("/leaders/{offset}/{limit}", apiHandler.CorsEnableMiddleware(apiHandler.AuthMiddleware(apiHandler.GetUser)))
-
-	router.AddHandlerDelete("/user", apiHandler.CorsEnableMiddleware(apiHandler.DeleteUser))
 
 	router.AddHandlerPost("/signup", apiHandler.CorsEnableMiddleware(apiHandler.AddUser))
 	router.AddHandlerPost("/signin", apiHandler.CorsEnableMiddleware(apiHandler.Authorize))
 
 	router.AddHandlerPut("/user", apiHandler.CorsEnableMiddleware(apiHandler.AuthMiddleware(apiHandler.UpdateUser)))
+	router.AddHandlerDelete("/user", apiHandler.CorsEnableMiddleware(apiHandler.DeleteUser))
 
 	//запускаем сервер
 	http.ListenAndServe(":8080", router)
